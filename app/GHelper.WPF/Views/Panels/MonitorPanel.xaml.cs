101afcfd7d658573cc9b1317a6ef7d893032f28c
bloodner <jacoblgardner@gmail.com>
2026-04-27 10:32:17 -0600

    feat: open G-Scope settings from gear button in panel header

--- app/GHelper.WPF/Views/Panels/MonitorPanel.xaml.cs
+++ app/GHelper.WPF/Views/Panels/MonitorPanel.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using GHelper.WPF.Views;
 
 namespace GHelper.WPF.Views.Panels
 {
@@ -7,6 +9,11 @@ namespace GHelper.WPF.Views.Panels
         public MonitorPanel()
         {
             InitializeComponent();
+        }
+
+        private void ScopeSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ScopeSettingsWindow.Show(Window.GetWindow(this));
         }
     }
 }
